acb09880bea0547f8c8845c0e82e649f750ac392
Roberto C. Torres <torres.roberto.c@gmail.com>
2018-01-30 10:21:56 -0600

    Specific genes filtered

--- bin_nt/BoleanPresenceAbsence.pl
+++ bin_nt/BoleanPresenceAbsence.pl
@@ -24,11 +24,11 @@ $List        = $ARGV[1];
 $MainPath    = $ARGV[2];
 
 my ($Project, $MainList, $PresenceAbsenceFile, $BoleanReport, $TotalQry,
-    $TotalPresenceAbsence, $Row, $Field, $BoleanInformativeReport, $Line,
-    $nColumns, $Count);
-my ($o, $i, $j);
+    $LinesOnPresenceAbsence, $ColumnsOnPresenceAbsence, $Row, $Field,
+    $BoleanInformativeReport, $Line, $nElements, $Count, $ORF);
+my ($i, $j);
 my (@List, @PresenceAbsence, @PresenceAbsenceFields, @PresenceAbsenceArray,
-    @Columns);
+    @Elements, @PresenceAbsenceMatrix);
 my $BoleanTable = [ ];
 my $GenesAnnotationReport = [ ];
 
@@ -38,36 +38,39 @@ $PresenceAbsenceFile     = $Project ."/". $ProjectName . "_Presence_Absence.csv"
 $BoleanReport            = $Project ."/". $ProjectName . "_Bolean_Presence_Absence.csv";
 $BoleanInformativeReport = $Project ."/". $ProjectName . "_Bolean_Informative_Presence_Absence.csv";
 
-#Loading the Presence/Absence genes file
-@List = ReadFile($MainList);
-$TotalQry = scalar@List;
-@PresenceAbsence = ReadFile($PresenceAbsenceFile);
-$TotalPresenceAbsence = scalar@PresenceAbsence;
+print "\nLoading the Presence/Absence file:";
+#@List = ReadFile($MainList);
+#$TotalQry = scalar@List;
+#@PresenceAbsence = ReadFile($PresenceAbsenceFile);
+#$TotalPresenceAbsence = scalar@PresenceAbsence;
+#
+#for ($i=0; $i<$TotalPresenceAbsence; $i++){
+#     $Row = $PresenceAbsence[$i];
+#     @PresenceAbsenceFields = split(",",$Row);
+#     chomp@PresenceAbsenceFields;
+#     $o = scalar@PresenceAbsenceFields;
+#     push (@PresenceAbsenceArray, [@PresenceAbsenceFields]);
+#     Progress($TotalPresenceAbsence,$i);
+#}
 
-for ($a=0; $a<$TotalPresenceAbsence; $a++){
-     $Row = $PresenceAbsence[$a];
-     @PresenceAbsenceFields = split(",",$Row);
-     chomp@PresenceAbsenceFields;
-     $o = scalar@PresenceAbsenceFields;
-     push (@PresenceAbsenceArray, [@PresenceAbsenceFields]);
-}
+($LinesOnPresenceAbsence, $ColumnsOnPresenceAbsence, @PresenceAbsenceMatrix) = Matrix($PresenceAbsenceFile);
 
-print "\nFormating the Absence\/Presence report to a bolean file...";
+print "\nTransforming the Presence/Absence into a bolean file:";
 
-$BoleanTable -> [0][0] = $PresenceAbsenceArray[0][0];
+$BoleanTable -> [0][0] = $PresenceAbsenceMatrix[0][0];
 
-for ($i=0; $i<$TotalPresenceAbsence; $i++){
-     $BoleanTable -> [$i][0] = $PresenceAbsenceArray[$i][0];
-     for ($j=0; $j<$o; $j++){
-          $BoleanTable -> [0][$j] = $PresenceAbsenceArray[0][$j];  
+for ($i=0; $i<$LinesOnPresenceAbsence; $i++){
+     $BoleanTable -> [$i][0] = $PresenceAbsenceMatrix[$i][0];
+     for ($j=0; $j<$ColumnsOnPresenceAbsence; $j++){
+          $BoleanTable -> [0][$j] = $PresenceAbsenceMatrix[0][$j];  
      }
 }
 
 #Setting the bolean data ("1" for presence and "0" for absence)
-for ($i=1; $i<$TotalPresenceAbsence; $i++){
-     for ($j=1; $j<$o; $j++){
-          if (defined $PresenceAbsenceArray[$i]->[$j]){          
-               $Field = $PresenceAbsenceArray[$i]->[$j];
+for ($i=1; $i<$LinesOnPresenceAbsence; $i++){
+     for ($j=1; $j<$ColumnsOnPresenceAbsence; $j++){
+          if (defined $PresenceAbsenceMatrix[$i]->[$j]){          
+               $Field = $PresenceAbsenceMatrix[$i]->[$j];
                if ($Field ne ""){   
                     $BoleanTable -> [$i][$j] = "1";
                }else{
@@ -77,34 +80,39 @@ for ($i=1; $i<$TotalPresenceAbsence; $i++){
                $BoleanTable -> [$i][$j] = "0";
           }
      }
+     Progress($LinesOnPresenceAbsence,$i);
 }
 
-print "\nWriting the bolean file...";
+print "\nWriting file...";
 open (FILE, ">$BoleanReport");
-for ($i=0; $i<$TotalPresenceAbsence; $i++){
-     for ($j=0; $j<$o; $j++){
+for ($i=0; $i<$LinesOnPresenceAbsence; $i++){
+     for ($j=0; $j<$ColumnsOnPresenceAbsence; $j++){
           print FILE $BoleanTable -> [$i][$j], ",";
      }
      print FILE "\n";
+     Progress($LinesOnPresenceAbsence,$i);
 }
 close FILE;
 
+print "\nWriting a bolean file only with the informative ORFs...";
 my @BoleanReport = ReadFile($BoleanReport);  
 open (FILE,">$BoleanInformativeReport");
         print FILE "$BoleanReport[0]\n";
-        for ($i=0; $i<scalar@BoleanReport;$i++){
+        for ($i=1; $i<scalar@BoleanReport;$i++){
                 $Line = $BoleanReport[$i];
-                @Columns = split(",",$Line);
-                chomp@Columns;
-                $nColumns = scalar@Columns;
+                @Elements = split(",",$Line);
+                chomp@Elements;
+                $ORF = $Elements[0];
+                shift@Elements;
+                $nElements = scalar@Elements;
 
                 $Count = 0;
-                foreach my $Element(@Columns){
+                foreach my $Element(@Elements){
                         if ($Element ne "0"){
                                 $Count++;
                         }
                 }
-                if($Count < $nColumns){
+                if($Count < $nElements && $Count > 1){
                         print FILE "$Line\n";   
                 }
         }
