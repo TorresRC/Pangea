1fb5889e485f2e7d8f82a153d981ccd9a28cab8c
Roberto C. Torres <rtorres@MacBook.Pro>
2020-06-20 19:59:47 -0500

    Make files to run subscripts in parallel

--- bin/Pangea.pl
+++ bin/Pangea.pl
@@ -2,7 +2,7 @@
 #################################################################################
 #                                                                               #
 # Programador:   Roberto C. Torres, Alfonso Méndez Tenorio                      #
-# Fecha:         2 de julio de 2017                                             #
+# Fecha:         July 2nd 2017                                                  #
 #################################################################################
 use strict;
 use Getopt::Long qw(GetOptions);
@@ -17,20 +17,22 @@ my $Usage    = "USAGE:\n  $0 [--help] [--project -p prefix] [--list -l filename]
       [--evalue -e evalue] [--ident -i percentage] [--cpus -t] [--out -o path]
 \n  Use \'--help\' to print detailed descriptions of options.\n\n";
 
-my ($ProjectName, $List, $eVal, $PIdent, $CPUs, $Help, $PanGenome, 
+my ($List, $eVal, $PIdent, $CPUs, $Help, $PanGenome, 
     $CoreGenome, $Boolean, $AnnotationPath, $MolType, $OutPath,
-    $Recovery);
+    $Recovery, $PCore);
 
 $Recovery = 0;
+$CPUs = 2;
+$PCore = 99;
 GetOptions(
         'help'           => \$Help,
-        'project|p=s'    => \$ProjectName,
         'list|l=s'       => \$List,
         'evalue|e=f'     => \$eVal,
         'ident|i=i'      => \$PIdent,
         'cpus|t=i'       => \$CPUs,
         'conpan|P'       => \$PanGenome,
         'alncore|C'      => \$CoreGenome,
+        'pcore|c=s'      => \$PCore,
         'booleantbl|b'   => \$Boolean,
         'annotation|a=s' => \$AnnotationPath,
         'moltype|m=s'    => \$MolType,
@@ -40,7 +42,6 @@ GetOptions(
 
 if($Help){
         print "
-        \t--project      <Project_Name>
         \t--list         <List_File_Name>
         \t--evalue       <e-value>
         \t--ident        <Percetage_of_Identity>
@@ -55,21 +56,27 @@ if($Help){
         exit;
 }
 
-print "\nProject: $ProjectName\nList: $List\nTrusted: $TrustedORFeome\ne: $eVal\nPI: $PIdent\nCPUs $CPUs\nMol type: $MolType\n\n";
+print "\nList: $List\ne: $eVal\nPI: $PIdent\nCPUs $CPUs\nMol type: $MolType\n\n";
 
-my ($Project, $FormatORFeomes, $SortGenes, $FilterORFeomes, $MakeBlastDb,
-    $InitialComparison, $GeneContent, $GeneContentPlot, $BooleanPresenceAbsence,
+my ($FormatORFeomes, $FilterORFeomes, $MakeBlastDb, $InitialComparison,
+    $PresenceAbsenceTbl, $GeneContent, $GeneContentPlot, $BooleanPresenceAbsence,
     $ConsensusSeq, $CoreAlign, $Start, $End, $Time, $RunTime, $Period,
     $ORFsFunctions, $PresenceAbsenceAnnotation, $PresenceAbsenceFile, $Core,
-    $ProgressFile, $GeneContentRScript);
+    $ProgressFile, $GeneContentRScript, $JobsInParallel, $FormatORFeomesStep,
+    $MakeBlastDbStep, $InitialComparisonStep, $CoreFeaturesAlignStep, $Query,
+    $Reference, $DataFiles, $FeaturesAlign);
+my ($i);
+my (@List);
 
 $Start = time();
 
 $FormatORFeomes            = $Src ."/". "FormatORFeomes.pl";
-$SortGenes                 = $Src ."/". "SortGenes.pl";
-$FilterORFeomes            = $Src ."/". "FilterORFeomes.pl";
+#$FilterORFeomes            = $Src ."/". "FilterORFeomes.pl";
 $MakeBlastDb               = $Src ."/". "MakeBlastDBs.pl";
 $InitialComparison         = $Src ."/". "InitialComparison.pl";
+$PresenceAbsenceTbl        = $Src ."/". "PresenceAbsenceTbl.pl";
+$FeaturesAlign             = $Src ."/". "FeaturesAlign.pl";
+
 $GeneContent               = $Src ."/". "GeneContent.pl";
 $GeneContentPlot           = $Src ."/". "GeneContentPlot.pl";
 $BooleanPresenceAbsence    = $Src ."/". "BooleanPresenceAbsence.pl";
@@ -78,37 +85,75 @@ $CoreAlign                 = $Src ."/". "CoreAlign.pl";
 $ORFsFunctions             = $Src ."/". "ORFsFunctions.pl";
 $PresenceAbsenceAnnotation = $Src ."/". "PresenceAbsenceAnnotation.pl";
 
-$ProgressFile          = $OutPath ."/". $ProjectName . '_Progress.csv';
-$GeneContentPlot       = $OutPath ."/". $ProjectName . "_GeneContentPlot.pdf";
-$GeneContentRScript    = $OutPath ."/". $ProjectName . "_GeneContentScript.R";
-$PresenceAbsenceFile   = $OutPath ."/". $ProjectName . "_Presence_Absence.csv";
-$Core                  = $OutPath ."/". $ProjectName . "_CoreGenome.csv";
+$ProgressFile          = $OutPath ."/". 'Progress.csv';
+$GeneContentPlot       = $OutPath ."/". "GeneContentPlot.pdf";
+$GeneContentRScript    = $OutPath ."/". "GeneContentScript.R";
+$PresenceAbsenceFile   = $OutPath ."/". "Presence_Absence.csv";
+$Core                  = $OutPath ."/". "CoreGenome.csv";
+
+$DataFiles             = $OutPath   ."/". "DataFiles";
+$FormatORFeomesStep    = $DataFiles ."/". "FormattingStep.temp";
+$MakeBlastDbStep       = $DataFiles ."/". "BlastDBsBuildingStep.temp";
+$InitialComparisonStep = $DataFiles ."/". "InitialComparisonStep.temp";
+$CoreFeaturesAlignStep = $DataFiles ."/". "CoreFeaturesAlignStep.temp";
+
+MakeDir($OutPath);
+MakeDir($DataFiles);
+@List = ReadFile($List);
+
+$JobsInParallel = $CPUs;
 
 if($Recovery == "0"){
-        system("perl $FormatORFeomes $ProjectName $List $AnnotationPath $MolType $OutPath");
-        system("perl $MakeBlastDb $ProjectName $List $MolType $OutPath");
-        system("perl $InitialComparison $ProjectName $List $MolType $eVal $PIdent $CPUs $OutPath");
-        system("perl $GeneContent $ProjectName $List $MolType $eVal $CPUs $OutPath $Recovery $AnnotationPath");
+    for ($i=0;$i<@List;$i++){
+        system("echo perl $FormatORFeomes $List[$i] $AnnotationPath $MolType $OutPath | cat >> $FormatORFeomesStep");
+        system("echo perl $MakeBlastDb $List[$i] $MolType $OutPath | cat >> $MakeBlastDbStep");
+        if ($i > 0){
+            system("echo perl $InitialComparison $List[$i] $List[0] $MolType $eVal $PIdent $CPUs $OutPath | cat >> $InitialComparisonStep");
+        }
+    }
+        
+    print "Formatting annotated features...";
+    system("cat $FormatORFeomesStep | parallel -j$JobsInParallel");
+    print "Done!";
+        
+    print "\nBuilding BLAST databases...";
+    system("cat $MakeBlastDbStep | parallel -j$JobsInParallel");
+    print "Done!";
+        
+    print "\nMake initial comparisons...";
+    system("cat $InitialComparisonStep | parallel -j$JobsInParallel");
+    print "Done!\n";
+    
+    system("perl $PresenceAbsenceTbl $List $PCore $OutPath");
+    
+    system("perl $FeaturesAlign $PCore $OutPath $CoreFeaturesAlignStep");
+    print "\nAligning core features...";
+    system("cat $CoreFeaturesAlignStep | parallel -j$JobsInParallel");
+    print "Done!\n";
+    
+    
+        
+        #system("perl $GeneContent $ProjectName $List $MolType $eVal $CPUs $OutPath $Recovery $AnnotationPath");
 }elsif($Recovery == "1"){
-        system("perl $GeneContent $ProjectName $List $MolType $eVal $CPUs $OutPath $Recovery $AnnotationPath");
+        #system("perl $GeneContent $ProjectName $List $MolType $eVal $CPUs $OutPath $Recovery $AnnotationPath");
 }
 
-system("perl $GeneContentPlot $ProgressFile $ProjectName $List $OutPath");
-system("perl $ORFsFunctions $ProjectName $AnnotationPath $PresenceAbsenceFile $OutPath");
-system("perl $PresenceAbsenceAnnotation $ProjectName PanGenes $AnnotationPath $PresenceAbsenceFile $OutPath");
-system("perl $PresenceAbsenceAnnotation $ProjectName CoreGenes $AnnotationPath $Core $OutPath");
-
-if($PanGenome){
-        system("perl $ConsensusSeq $ProjectName PanGenome $PresenceAbsenceFile $OutPath/ORFs $OutPath");
-}
-
-if($CoreGenome){
-        system("perl $CoreAlign $ProjectName 125 $Core $OutPath/ORFs $OutPath");
-}
-
-if($Boolean){
-        system("perl $BooleanPresenceAbsence $ProjectName $PresenceAbsenceFile $OutPath");
-}
+#system("perl $GeneContentPlot $ProgressFile $ProjectName $List $OutPath");
+#system("perl $ORFsFunctions $ProjectName $AnnotationPath $PresenceAbsenceFile $OutPath");
+#system("perl $PresenceAbsenceAnnotation $ProjectName PanGenes $AnnotationPath $PresenceAbsenceFile $OutPath");
+#system("perl $PresenceAbsenceAnnotation $ProjectName CoreGenes $AnnotationPath $Core $OutPath");
+#
+#if($PanGenome){
+#        system("perl $ConsensusSeq $ProjectName PanGenome $PresenceAbsenceFile $OutPath/ORFs $OutPath");
+#}
+#
+#if($CoreGenome){
+#        system("perl $CoreAlign $ProjectName 125 $Core $OutPath/ORFs $OutPath");
+#}
+#
+#if($Boolean){
+#        system("perl $BooleanPresenceAbsence $ProjectName $PresenceAbsenceFile $OutPath");
+#}
 
 #Timestamp
 $End = time();
@@ -124,5 +169,5 @@ if ($Time < 3600){
         $Period = "days";
 }
 
-print "\n\tFinished! The $ProjectName gene content analysis took $RunTime $Period\n\n";
+print "\n\tFinished! The analysis took $RunTime $Period\n\n";
 exit;
