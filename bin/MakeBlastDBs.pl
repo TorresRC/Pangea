c7af4fa7c982954f141b2358ac444c31b930fcb6
Roberto Torres <rtorres@MacBook-Pro.local>
2020-06-22 15:26:08 -0500

    Trusted option removed and in parallel running enabled

--- bin/MakeBlastDBs.pl
+++ bin/MakeBlastDBs.pl
@@ -4,40 +4,34 @@
 #                                                                               #
 #Programmer:    Roberto C. Torres                                               #
 #e-mail:        torres.roberto.c@gmail.com                                      #
-#Date:          10 de abril de 2017                                             #
+#Date:          April 10th 2017                                                 #
 #################################################################################
 use strict;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use Routines;
 
-my ($Usage, $ProjectName, $List, $Trusted, $MainPath, $MolType, $OutPath);
+my ($Usage, $Query, $MolType, $OutPath);
 
-$Usage = "\tUsage: MakeBlastDBs.pl <Project_Name> <List_File.ext> <Trusted_ORFeome.fasta> <Main_Path> <Molecule type>\n";
+$Usage = "\tUsage: MakeBlastDBs.pl <Query name> <Molecule type> <OutPath>\n";
 unless(@ARGV) {
         print $Usage;
         exit;
 }
 chomp @ARGV;
-$ProjectName    = $ARGV[0];
-$List           = $ARGV[1];
-$Trusted        = $ARGV[2];
-$MolType        = $ARGV[3];  # nucl or prot
-$OutPath        = $ARGV[4];
+$Query   = $ARGV[0];
+$MolType = $ARGV[1];  # nucl or prot
+$OutPath = $ARGV[2];
 
-my ($Project, $ORFeomesPath, $BlastPath, $TrustedORFeomeDb, $SeqExt,
-	$i, $n, $Qry, $InputFile, $Db, $cmd, $LogFile, $TrustedPrefix);
-my (@List);
+my ($BlastPath, $BlastDbPath, $SamplesFeaturesPath, $SeqExt, $InputFile, $Db,
+	$cmd, $LogFile, $TrustedPrefix);
 
-#$Project              = $OutPath ."/". $ProjectName;
-$Project = $OutPath;
-$BlastPath            = $OutPath ."/". "Blast";
-$ORFeomesPath         = $OutPath ."/". "ORFeomes";
+$LogFile = $OutPath ."/". "Errors.log";
 
-$TrustedPrefix        = Prefix($Trusted);
-$Trusted              = $ORFeomesPath ."/". $Trusted;
-$TrustedORFeomeDb     = $BlastPath ."/". $TrustedPrefix . "Db";
-$LogFile              = $OutPath ."/". $ProjectName . ".log";
+$SamplesFeaturesPath = $OutPath   ."/". "DataFiles" ."/". "SamplesFeatures";
+$BlastPath           = $OutPath   ."/". "DataFiles" ."/". "Blast";
+$BlastDbPath         = $BlastPath ."/". "DBs";
+
 
 if ($MolType eq "nucl"){
 	$SeqExt = ".ffn";
@@ -47,20 +41,12 @@ if ($MolType eq "nucl"){
 
 open (STDERR, "| tee -ai $LogFile") or die "$0: dup: $!";
 
-print "\nBuilding data bases:\n";
+MakeDir($BlastPath);
+MakeDir($BlastDbPath); 
 
-MakeDir($BlastPath); 
+$InputFile = $SamplesFeaturesPath ."/". $Query . $SeqExt;     
+$Db = $BlastDbPath ."/". $Query . "_BlastDb";
+system("makeblastdb -in $InputFile -dbtype $MolType -parse_seqids -out $Db >/dev/null 2>&1");
 
-@List = ReadFile($List); 
-$n = scalar@List;
-
-$cmd = `makeblastdb -in $Trusted -dbtype $MolType -parse_seqids -out $TrustedORFeomeDb`;
-
-for ($i=0; $i<$n; $i++){
-	$Qry = $List[$i];
-	$InputFile = $ORFeomesPath ."/". $Qry . $SeqExt;     
-	$Db = $BlastPath ."/". $Qry;
-	$cmd = `makeblastdb -in $InputFile -dbtype $MolType -parse_seqids -out $Db`;  
-	Progress($n, $i);
-}
+close STDERR;
 exit;
